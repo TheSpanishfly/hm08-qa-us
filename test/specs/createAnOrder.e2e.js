29f07df2c6c9f529b027391437dde2bf388886f2
AlexPerez <alexperez123@gmail.com>
2024-10-13 15:08:44 -0400

    New Version

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -1,23 +1,50 @@
-const page = require('../../page');
-const helper = require('../../helper')
+const page = require("../../page");
+const helper = require("../../helper");
 
-describe('Create an order', () => {
-    it('should open phone number modal', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumberButton = await $(page.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const pnoneNumberModal = await $(page.phoneNumberModal);
-        await expect(pnoneNumberModal).toBeExisting();
-    })
+describe("Create an order", () => {
+  it("should open phone number modal", async () => {
+    await browser.url(`/`);
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const phoneNumberButton = await $(page.phoneNumberButton);
+    await phoneNumberButton.waitForDisplayed();
+    await phoneNumberButton.click();
+    const pnoneNumberModal = await $(page.phoneNumberModal);
+    await expect(pnoneNumberModal).toBeExisting();
+  });
 
-    it('should save the phone', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumber = helper.getPhoneNumber("+1");
-        await page.submitPhoneNumber(phoneNumber);
-        await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-    })
-})
+  it("should save the phone", async () => {
+    await browser.url(`/`);
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const phoneNumber = helper.getPhoneNumber("+1");
+    await page.submitPhoneNumber(phoneNumber);
+    await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
+  });
 
+  it("should select the Supportive vehicle plan", async () => {
+    await browser.url(`/`);
+    await page.selectVehicleType("Supportive");
+    const selectedPlan = await $(page.selectedPlan);
+    await expect(selectedPlan).toBeExisting();
+  });
+
+  it("should add a credit card", async () => {
+    await browser.url(`/`);
+    await page.openAddCardModal();
+    await page.fillCardDetails("4242424242424242", "12/25", "123");
+    await page.focusOutFromCardCVV();
+    const linkButton = await $(page.linkButton);
+    await linkButton.waitForEnabled();
+    await linkButton.click();
+    const linkedCard = await $(page.linkedCard);
+    await expect(linkedCard).toBeExisting();
+  });
+
+  it("should write a message for the driver", async () => {
+    await browser.url(`/`);
+    await page.writeMessageForDriver("Please call me when you arrive.");
+    const messageField = await $(page.driverMessageField);
+    await expect(messageField).toHaveTextContaining(
+      "Please call me when you arrive."
+    );
+  });
+});
